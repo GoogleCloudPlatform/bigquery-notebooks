c892c5519feae1c75d402b42e6d4d32ca024bc4d
Lo Ferris <loferris@google.com>
2022-03-29 17:30:42 +0000

    adding owlbot_integration

--- owlbot.py
+++ owlbot.py
@@ -11,3 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import synthtool as s
+from synthtool import gcp
+from synthtool.languages import python
+
+python.python_notebooks_testing_pipeline()
+
